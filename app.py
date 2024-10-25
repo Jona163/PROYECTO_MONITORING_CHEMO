af09a8a14923d2db64dcd19363eaefa72b44e23e
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-25 10:41:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-
+from flask import Flask, request, jsonify, Response
+from flask_socketio import SocketIO
+from flask_cors import CORS
+import pyautogui
+import os
+import io
+from PIL import Image
+import ctypes

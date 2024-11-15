e84618e12dbb69e42ccc81fe1e3b6d35d7a96417
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-15 11:38:34 -0600

    Delete scripts directory

--- scripts/block_input.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -TypeDefinition @"
-using System;
-using System.Runtime.InteropServices;
-public class InputBlocker {
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern int BlockInput(bool fBlockIt);
-}
-"@
-[InputBlocker]::BlockInput($true)
